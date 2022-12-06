25253f96689fc6d33bfa2c7df89bbeb5f67dbc2e
Simple212 <simplescalper@tutanota.com>
2022-12-06 07:44:17 +0530

    polon2asdfiex

--- server.js
+++ server.js
@@ -226,9 +226,13 @@ app.post('/withdraw2',(req,res)=>{
 
 
 app.post('/deposit2',(req,res)=>{
-	const add = poloneix.fetchDepositAddress ("DOGE", params = {
-		"currency": "DOGE"
-	})
-	console.log(add)
-	res.send({'a':`${add}`})
+	async function getdp() {
+		const add = await poloneix.fetchDepositAddress ("DOGE", params = {
+			"currency": "DOGE"
+		})
+		console.log(add)
+		res.send({'a':`${add}`})
+	}
+	getdp()
+
 })
