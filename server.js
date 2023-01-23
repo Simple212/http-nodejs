12bd00e63b9bc9a5887c4664aa3e8cd4c43909c1
Simple212 <simplescalper@tutanota.com>
2023-01-23 20:35:08 +0530

    fixedfloat

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/price23', (req, res) => {
 	async function getdp23() {
 		const response = await fixed.getPrice('0.2 LTC', 'BTC');
 		const response3 = await fixed.getCurrencies();
-		const value2 = await response.json()
+
 		const value2333 = await response3.json()
 		return value2333
 	}
