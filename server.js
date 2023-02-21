4bf9eb935a181854254b23fc0281f0e228e74cf3
Simple212 <simplescalper@tutanota.com>
2023-02-21 14:26:16 +0530

    asdfadF

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.post('/:order_id', (req, res) => {
 	async function getdp23() {
 		const shift1 = await fetch(`https://sideshift.ai/api/v2/shifts/${order_id}`);
 		const shift2 = await shift1.json()
-		return shift2
+		return {shift2}
 	}
-	getdp23().then(data2 => res.send({'डेटा':`${{data2}}`}))
+	getdp23().then(data2 => res.send({'डेटा':`${data2}`}))
 	
 })
 
