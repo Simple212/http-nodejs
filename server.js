14dedf5fa68bc73058b7d96bbeeacdfb6e6ba9e6
Simple212 <simplescalper@tutanota.com>
2023-02-21 14:06:32 +0530

    asdfadF

--- server.js
+++ server.js
@@ -23,14 +23,16 @@ app.post('/price25', (req, res) => {
 })
 
 app.post('/:order_id', (req, res) => {
-	const {id} = req.params
-	async function getdp23() {
-		const shift1 = await fetch(`https://sideshift.ai/api/v2/shifts/${id}`);
-		const shift2 = await shift1.json() 
-		console.log(id)
-		console.log(shift2)
-	}
-	getdp23()
+	const {order_id} = req.params
+	console.log(order_id)
+// 	async function getdp23() {
+// 		const shift1 = await fetch(`https://sideshift.ai/api/v2/shifts/${id}`);
+// 		const shift2 = await shift1.json() 
+// 		console.log(id)
+// 		console.log(shift2)
+// 	}
+// 	getdp23()
+// 	
 // 	getdp23().then(data2 => res.send({'rate3':`${data2}`}))
 })
 
