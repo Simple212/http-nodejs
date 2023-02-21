84a58287aeea3dc695b46f6c56e8f4e2d6c1a2c1
Simple212 <simplescalper@tutanota.com>
2023-02-21 16:05:23 +0530

    asdfadF

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.post('/:order_id', (req, res) => {
 		res.send({
 			'id':`${id}`,
 		   'depositCoin':`${depositCoin}`,
+		   'createdAt':`${createdAt}`,
+		   'settleCoin':`${settleCoin}`,
+		   'depositAddress':`${depositAddress}`,
+		   'settleAddress':`${settleAddress}`,
 			'status':`${status}`,
 		   'type':`${type}`
 			
