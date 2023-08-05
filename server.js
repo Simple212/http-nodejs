2d52215c97e97dd6c3c0fefc8b0bbcb9b20c7bd5
Simple212 <simplescalper@tutanota.com>
2023-08-05 11:27:24 +0530

    asdfasdf

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.post('/order25', (req, res) => {
 	console.log(req.body.to)
 	console.log(req.body.amount)
 	async function getdp23() {
-		const shift = await fetch(`https://sideshift.ai/api/v2/shifts/fixed`,{
+		const shift = await fetch(`https://sideshift.ai/api/v2/shifts/variable`,{
 			'method':'post',
 			'headers':{'Content-type':'application/json'},
 			'body': JSON.stringify({
@@ -77,13 +77,12 @@ app.post('/order25', (req, res) => {
 				"affiliateId": "NpecndnG94",
 				"depositCoin": `${req.body.from}`,
 				"settleCoin": `${req.body.to}`,
-				"commissionRate": "0.005",
-			    "depositAmount": `${req.body.amount}`
+				"commissionRate": "0.005"
 			})
 		})
 		const shift2 = await  shift.json()
 		const address_to_send = 
-		res.send({'address':`${shift2.depositAddress}`,'id':`${shift2.id}`,'amount1':`${shift2.depositAmount}`})
+		res.send({'address':`${shift2.depositAddress}`,'id':`${shift2.id}`,'amount1':`${req.body.amount}`})
 }
 getdp23()
 })
